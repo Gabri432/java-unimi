09a1f9737e05dda73e015b48a8088207e160e871
Gabri432 <gabrielegatti432@gmail.com>
2023-05-01 22:57:31 +0200

    Increasing world size.

--- basic_game/main/GamePanel.java
+++ basic_game/main/GamePanel.java
@@ -19,8 +19,8 @@ public class GamePanel extends JPanel implements Runnable {
     public final int screenWidth = tileSize * maxScreenCol;
     public final int screenHeight = tileSize * maxScreenRow;
 
-    public final int maxWorldCol = 30;
-    public final int maxWorldRow = 30;
+    public final int maxWorldCol = 50;
+    public final int maxWorldRow = 50;
     public final int worldWidth = tileSize * maxWorldCol;
     public final int worldHeight = tileSize * maxWorldRow;
 

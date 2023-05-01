51a3bc582898d75075e62c41161c7cd5f787ec19
Gabri432 <gabrielegatti432@gmail.com>
2023-05-01 17:30:23 +0200

    Creating and using player assets.

--- basic_game/entity/Entity.java
+++ basic_game/entity/Entity.java
@@ -1,5 +1,10 @@
 package basic_game.entity;
 
+import java.awt.image.BufferedImage;
+
 public class Entity {
     public int x, y, speed;
+
+    public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    public String direction;
 }
--- basic_game/entity/Player.java
+++ basic_game/entity/Player.java
@@ -1,7 +1,11 @@
 package basic_game.entity;
 
-import java.awt.Color;
+//import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import basic_game.main.GamePanel;
 import basic_game.main.KeyHandler;
@@ -15,30 +19,69 @@ public class Player extends Entity {
         this.keyH = keyH;
 
         setDefaultValues();
+        getPlayerImage();
     }
 
     public void setDefaultValues() {
         x = 100;
         y = 100;
         speed = 4;
+        direction = "down";
+    }
+    public void getPlayerImage() {
+        try {
+            up1 = ImageIO.read(getClass().getResourceAsStream("player_assets/player_up_image-1.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("player_assets/player_down_image-1.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("player_assets/player_left_image-1.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("player_assets/player_right_image-1.png"));
+
+            up2 = ImageIO.read(getClass().getResourceAsStream("player_assets/player_up_image-2.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("player_assets/player_down_image-2.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("player_assets/player_left_image-2.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("player_assets/player_right_image-2.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void update() {
         if (keyH.upPressed) {
+            direction = "up";
             y -= speed;
         }
         if (keyH.downPressed) {
+            direction = "down";
             y += speed;
         }
         if (keyH.leftPressed) {
+            direction = "left";
             x -= speed;
         }
         if (keyH.rightPressed) {
+            direction = "right";
             x += speed;
         }
     }
     public void draw(Graphics2D g2) {
-        g2.setColor(Color.WHITE);
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        //g2.setColor(Color.WHITE);
+        //g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        BufferedImage image = null;
+        switch(direction) {
+            case "up":
+            image = up1;
+            break;
+            case "down":
+            image = down1;
+            break;
+            case "left":
+            image = left1;
+            break;
+            case "right":
+            image = right1;
+            break;
+            default:
+        }
+
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
     }
 }

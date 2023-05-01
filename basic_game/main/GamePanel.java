0258e13c250841aae796b31bad771ef4cca0ec68
Gabri432 <gabrielegatti432@gmail.com>
2023-05-01 13:39:40 +0200

    Creating the game panel.

---
+++ basic_game/main/GamePanel.java
@@ -0,0 +1,21 @@
+package basic_game.main;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel {
+    final int originalTileSize = 16;
+    final int scale = 3;
+    final int tileSize = originalTileSize * scale;
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol;
+    final int screenHeight = tileSize * maxScreenRow;
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.BLACK);
+        this.setDoubleBuffered(true);
+    }
+}

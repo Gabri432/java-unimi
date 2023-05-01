a024ef5e11b4ac6270272c3ecea3684f6e1340c1
Gabri432 <gabrielegatti432@gmail.com>
2023-05-01 20:56:26 +0200

    'loadMap' function will take a string as input.

--- basic_game/tile/TileManager.java
+++ basic_game/tile/TileManager.java
@@ -20,7 +20,7 @@ public class TileManager {
         tile = new Tile[10];
         mapTileNum = new int[gp.maxScreenCol][gp.maxScreenRow];
         getTileImage();
-        loadMap();
+        loadMap("assets/maps/map1.txt");
     }
 
     public void getTileImage() {
@@ -38,9 +38,9 @@ public class TileManager {
         }
     }
 
-    public void loadMap() {
+    public void loadMap(String map) {
         try {
-            InputStream is = getClass().getResourceAsStream("assets/maps/map1.txt");
+            InputStream is = getClass().getResourceAsStream(map);
             BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
             int col = 0;

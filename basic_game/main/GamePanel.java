82ee8693401e8d0c4a3c6b58862b6cfc375b0c3c
Gabri432 <gabrielegatti432@gmail.com>
2023-05-01 14:18:10 +0200

    Creating a function to update the components on the screen.

--- basic_game/main/GamePanel.java
+++ basic_game/main/GamePanel.java
@@ -2,9 +2,12 @@ package basic_game.main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
     final int originalTileSize = 16;
     final int scale = 3;
     final int tileSize = originalTileSize * scale;
@@ -13,9 +16,34 @@ public class GamePanel extends JPanel {
     final int screenWidth = tileSize * maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
 
+    Thread gameThread;
+
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        while(gameThread != null) {
+            update();
+            repaint();
+        }
+    }
+
+    public void update() {}
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setColor(Color.WHITE);
+        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.dispose();
     }
 }

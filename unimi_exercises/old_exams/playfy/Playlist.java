42459a343bf7e7ca63f671fa01474d152b9292e6
Gabri432 <gabrielegatti432@gmail.com>
2023-06-18 17:04:46 +0200

    Adding specifications to Playlist class methods.

--- unimi_exercises/old_exams/playfy/Playlist.java
+++ unimi_exercises/old_exams/playfy/Playlist.java
@@ -1,25 +1,71 @@
 import java.util.HashSet;
 import java.util.LinkedList;
 
+/**
+ * Mutable class that represents a Playlist given its name and a list of songs. 
+ * A playlist can be empty and songs can be added and removed after its creation.
+ */
 public class Playlist {
     public int duration;
     public LinkedList<Song> songs;
     public final String title;
 
+    //RI: title cannot be or empty null.
+    //title must be at least one character long.
+
+    //AB: a list of song objects will represent the list of songs of a playlist.
+    //The ith item of the list is the ith song of the playlist.  
+
+    /**
+     * It constructs a Playlist object given its title.
+     * @param title a String representing the Playlist title.
+     * @throws NullPointerException if the title is null.
+     * @throws IllegalArgumentException if the title is not at least 1 character long.
+     */
     public Playlist(final String title) {
         this.title = title;
     }
 
+    /**
+     * It constructs a Playlist object given its title and an initial list of songs.
+     * @param title a String representing the Playlist title.
+     * @param songs a LinkedList of Song objects representing the songs of the playlist.
+     * @throws NullPointerException if the title is null or the list of songs is null.
+     * @throws IllegalArgumentException if the title is not at least 1 character long.
+     */
     public Playlist(final String title, LinkedList<Song> songs) {
         this.title = title;
         LinkedList<Song> myList = new LinkedList<>();
         while (songs.iterator().hasNext()) {
             myList.add(songs.iterator().next());
         }
+        this.songs = myList;
     }
 
-    public void addSong() {}
-    public void removeSong() {}
+    /**
+     * Adds a new song to the playlist, if it is not already in it.
+     * @param song a Song object that represents the song to add to the playlist.
+     * @throws NullPointerException if the song is null.
+     */
+    public void addSong(Song song) {
+        if (!this.songs.contains(song)) this.songs.add(song);
+    }
+
+    /**
+     * Removes a song from the playlist, if found in the playlist.
+     * @param song a Song object that represents the song to remove from the playlist.
+     * @throws NullPointerException if the song is null.
+     */
+    public void removeSong(Song song) {
+        if (this.songs.contains(song)) this.songs.remove(song);
+    }
+
+    /**
+     * Prints all the songs belonging to an album.
+     * @param albumTitle a String representing the title of the album of the desired songs.
+     * @throws NullPointerException if the album title is a null string.
+     * @throws IllegalArgumentException if the album is a empty string.
+     */
     public void printSongs(String albumTitle) {
         while (songs.iterator().hasNext()) {
             Song currentSong = songs.iterator().next();
@@ -29,6 +75,9 @@ public class Playlist {
         }
     }
     
+    /**
+     * Prints all the different albums that are present in the playlist.
+     */
     public void printAlbums() {
         HashSet<String> setOfAlbumTitles = new HashSet<>();
         while (songs.iterator().hasNext()) {

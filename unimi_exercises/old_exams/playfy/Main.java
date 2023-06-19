41627de51d87c910a494ed4e7e2e9270ae377c6f
Gabri432 <gabrielegatti432@gmail.com>
2023-06-19 16:10:17 +0200

    Adding some println functions to understand the loop course.

--- unimi_exercises/old_exams/playfy/Main.java
+++ unimi_exercises/old_exams/playfy/Main.java
@@ -6,47 +6,64 @@ public class Main {
         Scanner myObj = new Scanner(System.in);
         LinkedList<Album> albums = new LinkedList<>();
         Playlist fusa = new Playlist("Fusa");
+        System.out.println("PlayFy running...");
         while (myObj.hasNextLine()) {
             String inputLine = myObj.nextLine();
             if (inputLine.contains("ALBUM")) {
                 LinkedList<Song> songs = new LinkedList<>();
                 String albumTitle = inputLine.split(" ", 2)[1];
-                while (myObj.hasNextLine()) {
-                    if (inputLine.contains("-")) {
-                        String[] songsDetails = inputLine.split("-");
+                System.out.println("CREATION OF ALBUM '"+albumTitle+"': Insert songs");
+                Scanner albumScanner = new Scanner(System.in);
+                while (albumScanner.hasNextLine()) { //Getting album details
+                    String albumLine = albumScanner.nextLine();
+                    if (albumLine.contains("-")) {
+                        String[] songsDetails = albumLine.split("-");
                         Song currentSong = new Song(songsDetails[1], fromFormatToSeconds(songsDetails[0]), albumTitle);
                         songs.add(currentSong);
+                        System.out.println("Song inserted");
                     }
-                    if (inputLine.equals(".")) break;
+                    if (albumLine.length() == 1) break;
                 }
+                System.out.println("Exiting loop");
+                albumScanner.close();
                 Album myAlbum = new Album(albumTitle, songs);
                 System.out.println(myAlbum);
                 albums.add(myAlbum);
             }
             if (inputLine.contains("PLAYLIST")) {
+                System.out.println("PLAYLIST CREATION: Insert songs");
                 String playlistTitle = inputLine.split(" ", 2)[1];
                 Playlist myPlaylist = new Playlist(playlistTitle);
-                while (myObj.hasNextLine()) {
-                    if (inputLine.contains("-")) {
-                        String[] songDetails = inputLine.split(" ");
+                Scanner playlistScanner = new Scanner(System.in);
+                while (playlistScanner.hasNextLine()) { //Getting playlist details
+                    String playlistLine = playlistScanner.nextLine();
+                    if (playlistLine.contains("-")) {
+                        String[] songDetails = playlistLine.split(" ");
                         int albumPosition = Integer.parseInt(songDetails[0]);
                         int songPosition = Integer.parseInt(songDetails[1]);
                         Song currentSong = albums.get(albumPosition).songs.get(songPosition);
                         myPlaylist.addSong(currentSong);
                         fusa.addSong(currentSong);
                     }
-                    if (inputLine.equals(".")) break;
+                    if (playlistLine.length() == 1) break;
                 }
+                System.out.println("Exiting loop");
+                playlistScanner.close();
                 System.out.println(myPlaylist);
             }
         }
+        myObj.close();
         System.out.println(fusa);
         System.out.println(albums);
-        myObj.close();
     }
 
     public static int fromFormatToSeconds(String time) {
-        String[] timeDetails = time.split(":");
+        String[] timeDetails = time.trim().split(":");
+        if (timeDetails.length == 2) {
+            int seconds = Integer.parseInt(timeDetails[1]);
+            int minutesInSeconds = Integer.parseInt(timeDetails[0]) * 60;
+            return seconds+minutesInSeconds;
+        }
         int seconds = Integer.parseInt(timeDetails[2]);
         int minutesInSeconds = Integer.parseInt(timeDetails[1]) * 60;
         int hoursInSeconds = Integer.parseInt(timeDetails[1]) * 3600;

4180218244e63c8a8c8849e71ab5a75921970f4f
Gabri432 <gabrielegatti432@gmail.com>
2023-06-19 19:28:21 +0200

    Adding 'price' property to the Toy class.

--- unimi_exercises/old_exams/bancarelle/Toy.java
+++ unimi_exercises/old_exams/bancarelle/Toy.java
@@ -3,29 +3,32 @@ package unimi_exercises.old_exams.bancarelle;
 import java.util.Objects;
 
 /**
- * Immutable class representing a toy given by its name and material.
+ * Immutable class representing a toy given by its name, material and price.
  */
 public class Toy {
     final public String name;
     final public String material;
+    final public float price;
 
-    //RI: name and material must be non-null and non-empty strings.
-    //AF: a toy is represented by two properties, name and material.
+    //RI: name and material must be non-null and non-empty strings. Price must be at least 0.01.
+    //AF: a toy is represented by three properties: name, material and price.
 
     /**
      * It constructs a Toy object given its name and material.
      * @param name a String representing the name of the toy.
      * @param material a String representing the material of the toy.
      * @throws NullPointerException if either toy name or toy material is null.
-     * @throws IllegalArgumentException if either toy name or toy material is less than 1 character long.
+     * @throws IllegalArgumentException if either toy name or toy material is less than 1 character long or the price is lower than 0.01.
      */
-    Toy(String name, String material) {
+    public Toy(String name, String material, float price) {
         Objects.requireNonNull(name, "Toy name cannot be null");
         Objects.requireNonNull(material, "Toy material cannot be null");
         if (name.trim().length() < 1) throw new IllegalArgumentException("Toy name must contain at least one character.");
         if (material.trim().length() < 1) throw new IllegalArgumentException("Toy material must contain at least one character.");
+        if (price < 0.01) throw new IllegalArgumentException("Toy price must be at least 0.01.");
         this.name = name;
         this.material = material;
+        this.price = price;
     }
 
     @Override

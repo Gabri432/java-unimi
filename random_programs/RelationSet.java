bb91fbf5d1c20944d27e40a46fd299dc74e5de6b
Gabri432 <gabrielegatti432@gmail.com>
2023-06-20 21:57:48 +0200

    Creating the RelationSet class.

---
+++ random_programs/RelationSet.java
@@ -0,0 +1,120 @@
+package random_programs;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * The RelationChecker offers a set of methods to classify the property of a set of relationships. <br /><br />
+ * DISCLAIMER: It does <strong>not</strong> guarantee 100% accuracy. I do not recommend using it as a tool for your homeworks.
+ */
+public class RelationSet {
+    public final Set<Relation> relations; //The set of relations. Like R = {(a, a), (a, b), ...}.
+    public final Set<Integer> distinctElements;
+
+    /**
+     * A Relation class object represents a relation between two elements a and b.
+     */
+    public class Relation {
+        public final int a;
+        public final int b;
+        
+
+        /**
+         * It creates a relation object.
+         * @param a an Integer.
+         * @param b an Integer.
+         */
+        public Relation(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        @Override
+        public String toString() {
+            return "( + " +  a +" "+ b + ")";
+        }
+    }
+
+    /**
+     * It create a set of relations.
+     * @throws NullPointerException if the set of relations is null.
+     */
+    public RelationSet(Set<Relation> relations) {
+        Objects.requireNonNull(relations, "The set of relations cannot be null.");
+        //Set<Relation> relations = new HashSet<>();
+        //relations.add(new Relation(1, 1)); //(1, 1)
+        //relations.add(new Relation(2, 2)); //(2, 2)
+        //relations.add(new Relation(3, 3)); //(3, 3)
+        this.relations = relations;
+        Set<Integer> distinctElements = new HashSet<>();
+        for (var relation : relations) {
+            distinctElements.add(relation.a);
+            distinctElements.add(relation.b);
+        }
+        this.distinctElements = distinctElements;
+    }
+
+    //(x , y) in Set and (y, z) in Set => (x, z) in Set (x, y and z not necessarly different).
+    public boolean isTransitive() {
+        if (distinctElements.size() < 2) return true; //vacuous truth.
+        for (var relation : relations) {
+            for (var element : distinctElements) {
+                if (relations.contains(new Relation(relation.b, element)) && 
+                    !relations.contains(new Relation(relation.a, element))) return false;
+            }
+        }
+        return true;
+    }
+
+    //For every x in Set => (x, x) in Set.
+    public boolean isReflexive() {
+        if (distinctElements.size() < 2) return true; //vacuous truth.
+        for (var element : distinctElements) {
+            if (!this.relations.contains(new Relation(element, element))) return false;
+        }
+        return true;
+    }
+
+    //For every (x, y) in Set => (y, x) in Set.
+    public boolean isSymmetric() {
+        if (distinctElements.size() < 2) return true; //vacuous truth.
+        for (var relation : relations) {
+            if (!relations.contains(new Relation(relation.b, relation.a))) return false;
+        }
+        return true;
+    }
+
+    //For every (x, y) in Set => (y, x) not in Set unless x == y.
+    public boolean isAntiSymmetric() {
+        if (distinctElements.size() < 2) return true; //vacuous truth.
+        for (var relation : relations) {
+            if (relations.contains(new Relation(relation.b, relation.a)) &&
+               relation.b != relation.a) return false;
+        }
+        return true;
+    }
+
+    public boolean isEquivalence() {
+        if (this.isTransitive() && this.isReflexive() && this.isSymmetric()) return true;
+        return false;
+    }
+
+    public boolean isOrder() {
+        if (this.isTransitive() && this.isReflexive() && this.isAntiSymmetric()) return true;
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append("Is the set: " + relations + ", a\n");
+        s.append("symmetric relation? "+ (isSymmetric() ? "Yes" : "no") +"\n");
+        s.append("antisymmetric relation? "+ (isAntiSymmetric() ? "Yes" : "no") + "\n");
+        s.append("reflexive relation? "+ (isReflexive() ? "Yes" : "no") +"\n");
+        s.append("transitive relation? "+ (isTransitive() ? "Yes" : "no") +"\n");
+        s.append("equivalence relation? "+ (isEquivalence() ? "Yes" : "no") +"\n");
+        s.append("order relation? "+ (isOrder() ? "Yes" : "no") +"\n");
+        return s.toString();
+    }
+}

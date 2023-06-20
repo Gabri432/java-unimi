b31c94973fb3c91b065b8ce25b9b291a656cf2db
Gabri432 <gabrielegatti432@gmail.com>
2023-06-20 22:59:15 +0200

    Adding other test cases.

--- random_programs/Main.java
+++ random_programs/Main.java
@@ -2,10 +2,34 @@ package random_programs;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(transitiveRel());
+        System.out.println(reflexiveRel());
+        System.out.println(antisymmetricRel());
+    }
+
+    static public RelationSet transitiveRel() {
         RelationSet myRelationSet = new RelationSet();
         myRelationSet.addRelation(1, 2);
         myRelationSet.addRelation(2, 3);
         myRelationSet.addRelation(1, 3);
-        System.out.println(myRelationSet);
+        return myRelationSet;
+    }
+
+    static public RelationSet reflexiveRel() {
+        RelationSet myRelationSet = new RelationSet();
+        myRelationSet.addRelation(1, 1);
+        myRelationSet.addRelation(2, 2);
+        myRelationSet.addRelation(3, 3);
+        myRelationSet.addRelation(4, 4);
+        return myRelationSet;
+    }
+
+    static public RelationSet antisymmetricRel() {
+        RelationSet myRelationSet = new RelationSet();
+        myRelationSet.addRelation(1, 2);
+        myRelationSet.addRelation(2, 2);
+        myRelationSet.addRelation(3, 1);
+        myRelationSet.addRelation(4, 4);
+        return myRelationSet;
     }
 }

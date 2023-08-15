4c2a1c3c279c947e6e929d0aedc572bea92070f4
Gabri432 <gabrielegatti432@gmail.com>
2023-08-15 14:49:27 +0200

    Mapping currency names and values-to-euros.

--- gui/currencyConverter/OptionList.java
+++ gui/currencyConverter/OptionList.java
@@ -2,6 +2,7 @@ package gui.currencyConverter;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
 
 import javax.swing.JComboBox;
 
@@ -26,16 +27,24 @@ public class OptionList implements ActionListener {
         @SuppressWarnings("unchecked")
         JComboBox<String> cb = (JComboBox<String>)e.getSource();
         String currencyName = (String)cb.getSelectedItem();
-        getCurrency(currencyName);
+        getCurrency(currencyName.split(" ")[0]);
     }
 
     /**
-     * Returns the respective currency value in cents of euros.
+     * Returns the corresponding currency value in thousandths of euros. The lowest value returned will be 1.
      * @param currencyName the currency name: USD, EUR, ...
-     * @return the value of the selected currency in EUR cents. <br /><br /> Example: getCurrency("USD") => 0.80 (in EUR)
+     * @return the value of the selected currency in EUR cents. <br /><br /> Example: getCurrency("USD") => 920 (which is 0.92 in EUR)
      */
     public int getCurrency(String currencyName) {
-        return 0;
+        HashMap<String, Integer> currencyMap = new HashMap<String, Integer>(); 
+        currencyMap.putIfAbsent("EUR",1000);
+        currencyMap.putIfAbsent("USD",920);
+        currencyMap.putIfAbsent("JPY",6);
+        currencyMap.putIfAbsent("GBP",116);
+        currencyMap.putIfAbsent("CHF",104);
+        currencyMap.putIfAbsent("RUB",9);
+        currencyMap.putIfAbsent("CNY",13);
+        return currencyMap.get(currencyName);
     }
     
 }

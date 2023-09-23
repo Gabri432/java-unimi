4f3f3c24468472c33db205ab22b411e58ee13807
Gabri432 <gabrielegatti432@gmail.com>
2023-09-23 17:06:59 +0200

    Commenting class 'Valuta' methods.

--- unimi_exercises/old_exams/cambioValuta/Valuta.java
+++ unimi_exercises/old_exams/cambioValuta/Valuta.java
@@ -6,10 +6,15 @@ public class Valuta {
     public final String nome;
     public final String simbolo;
 
+    //RI: nome e simbolo sono non-vuoti, non-nulli e immutabili.
+    //AF: Valuta è una classe rappresentata da due proprietà di tipo String, nome e simbolo, immutabili.
+
     /**
-     * 
-     * @param nome
-     * @param simbolo
+     * Crea un oggetto di tipo Valuta in base al suo nome e al suo simbolo.
+     * @param nome stringa che rappresenta il nome della valuta.
+     * @param simbolo stringa che rappresenta il simbolo della valuta.
+     * @throws NullPointerException se nome o simbolo sono nulli.
+     * @throws IllegalArgumentException se nome o simbolo sono vuoti.
      */
     public Valuta(String nome, String simbolo) {
         if (Objects.requireNonNull(nome,  "Nome non può essere nullo.").isEmpty()) {
@@ -20,6 +25,26 @@ public class Valuta {
         }
         this.nome = nome;
         this.simbolo = simbolo;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+
+        if (!(obj instanceof Valuta)) {
+            return false;
+        }
+        Valuta valuta = (Valuta) obj;
+        if (valuta.nome != this.nome) return false;
+        if (valuta.simbolo != this.simbolo) return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, simbolo);
     }
 
     @Override

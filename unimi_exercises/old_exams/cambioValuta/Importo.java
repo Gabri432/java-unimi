d2bd2624e2d19b80d8ed89559889c8035b93771a
Gabri432 <gabrielegatti432@gmail.com>
2023-09-23 17:17:46 +0200

    Adding methods.

--- unimi_exercises/old_exams/cambioValuta/Importo.java
+++ unimi_exercises/old_exams/cambioValuta/Importo.java
@@ -1,4 +1,32 @@
 package unimi_exercises.old_exams.cambioValuta;
 
+import java.util.Objects;
+
 public class Importo {
+    private float valore;
+    private final Valuta valuta;
+    
+    public Importo(float valore, Valuta valuta) {
+        this.valore = valore;
+        this.valuta = valuta;
+    }
+    
+    public float sommaAlgebrica(float quantità) {
+        return 0;
+    }
+
+    public float somma() {
+        return 0;
+    }
+
+    public float sottrai() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return this.valuta.simbolo + " " + this.valore;
+    }
+
+    
 }
